384b39c286d803bd1174f1fba1f878e8b61c66f4
Blazespireyt <oubenjad@gmail.com>
2026-07-07 09:12:36 +0100

    Initial Archos Access 70 3G TWRP device tree

---
+++ omni_ac70cr3gv2.mk
@@ -0,0 +1,30 @@
+#
+# Copyright 2018 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Embedded
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_COPY_FILES += \
+    device/archos/ac70cr3gv2/recovery.fstab:recovery/root/etc/recovery.fstab
+
+PRODUCT_DEVICE := ac70cr3gv2
+PRODUCT_NAME := omni_ac70cr3gv2
+PRODUCT_BRAND := DOOGEE
+PRODUCT_MODEL := Archos Access 70 3G
+PRODUCT_MANUFACTURER := DOOGEE
